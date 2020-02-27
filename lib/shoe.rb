98090c886e543ce1825cb16a499f1429732688a2
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-27 13:55:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,6 +10,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
 
   def cobble
-      puts "Your shoe is as good as new!"
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+
   end
 end
